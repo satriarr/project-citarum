4f5b988dfe2b3e221a06a8053e2128961a509ea7
Muhamad Diaz <muhamaddiaz10@gmail.com>
2019-07-17 10:19:01 +0700

    Updates

---
+++ precache-manifest.dfa47e3057d4e1adbb85f010ee8292f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d90a4625e889ec9965f6d06806036a",
+    "url": "/project-citarum/index.html"
+  },
+  {
+    "revision": "189407c2e5b679c1c4cd",
+    "url": "/project-citarum/static/css/main.b9e55eb1.chunk.css"
+  },
+  {
+    "revision": "9f9d18b1bfb391effa70",
+    "url": "/project-citarum/static/js/2.8db65166.chunk.js"
+  },
+  {
+    "revision": "189407c2e5b679c1c4cd",
+    "url": "/project-citarum/static/js/main.3d00d182.chunk.js"
+  },
+  {
+    "revision": "f979ee2b87d8ad2a5073",
+    "url": "/project-citarum/static/js/runtime~main.3f1b6642.js"
+  },
+  {
+    "revision": "11b9f9658ba93b410efeb0cb4601de90",
+    "url": "/project-citarum/static/media/bandung.11b9f965.jpg"
+  },
+  {
+    "revision": "87d71dded32856c7d029de02648b9a6f",
+    "url": "/project-citarum/static/media/profile.87d71dde.jpg"
+  }
+]);
